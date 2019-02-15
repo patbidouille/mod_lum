9945c19a0d4946fefa863ce6038630e036f06979
patbidouille <patbidouille@free.fr>
2019-02-15 12:01:10 +0100

    Vers1.2

--- mod_lum.ino
+++ mod_lum.ino
@@ -21,12 +21,26 @@
  Module tricapteur:
  http://arduinolearning.com/code/htu21d-bmp180-bh1750fvi-sensor-example.php
  
+ Commande et conf MQTT
+ - mod_lum/conf = Défini le seuil de luminosité bas
+ - mod_lum/cmd  = 
+    si mesg == "ON" 	On allume la led
+    si mesg == "OFF" 	On éteint la led
+ - mod_lum/haut = si mesg == "ON" on monte les volets
+ - mod_lum/bas = si mesg == "ON" on baisse les volets
+ - mod_lum/cmd = 
+    si mesg == "aff" On renvoi la valeur de conf luminosité topic MQTT "mod_lum/conflum"  
+    si mesg == "tmp" On renvoi le valeur de conf du temps topic MQTT mod_lum/conflum"  
+  - mod_lum/conftemps Défini la valeur de temps entre 2 mesures.
+ 
  
 */
  
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <EEPROM.h>
+//#include <PString.h>
 
 // DHT 11 sensor
 #define DHTPIN 2
@@ -35,11 +49,11 @@
 // Utilisation d’une photo-résistance 
 // Et ports pour cmd volet
 const int port = A0;    // LDR
-#define haut 12		// port de commande des volets
+#define haut 13		// port de commande des volets
 //#define arret 13
 #define bas 15
 //#define lbp 14
-#define lbp 13
+#define lbp 12
  
 // Update these with values suitable for your network.
 // Buffer pour convertir en chaine de l'adresse IP de l'appareil
@@ -71,19 +85,21 @@ DHT dht(DHTPIN, DHTTYPE, 15);
 // Variables
 int valeur = 0;         // temperature
 float vin = 0;          // voltage capteur
-char msg[50];           // message mqtt
+char msg[100];          // Buffer pour envoyer message mqtt
 int value = 0;
 unsigned long readTime;
-//Buffer qui permet de décoder les messages MQTT reçus
-char message_buff[100];
+char floatmsg[10];      // Buffer pour les nb avec virgules
+char message_buff[100]; // Buffer qui permet de décoder les messages MQTT reçus
 long lastMsg = 0;       // Horodatage du dernier message publié sur MQTT
 long lastbas = 0;       // compteur entre 2 lecture de capteur
-bool debug = true;      // Affiche sur la console si True
+bool debug = true;     // Affiche sur la console si True
 bool mess = false;      // true si message reçu
 String sujet = "";      // contient le topic
 String mesg = "";       // contient le message reçu
-long lum = 30;          // Valeur de luminosité par défaut
+int lum = 30;           // Valeur de luminosité par défaut
+long lsmg = 60000;       // Valeur de temps entre 2 lectures
 bool enbas = false;     // Flag de test volet bas
+unsigned int addr = 0;  // addresse de stockage de lum
  
 //========================================
 void setup() {
@@ -100,6 +116,45 @@ void setup() {
   client.setCallback(callback);
   dht.begin();                      // initialize temperature sensor
   client.subscribe("mod_lum");
+
+  EEPROM.begin(512);
+  
+  lum=eeGetInt(100);
+  if ((lum == 0)|| (lsmg == -1))  {
+    lum=60;
+  }
+  lsmg=eeGetInt(50);
+  if ((lsmg == 0) || (lsmg == -1)) {
+    lsmg=10000;
+  }
+
+  if (debug) {
+    Serial.print("luminosité enregistrée ");
+    Serial.println(lum);
+    Serial.print("temps enregistré ");
+    Serial.println(lsmg);
+  }
+}
+
+//========================================
+void eeWriteInt(int pos, int val) {
+    byte* p = (byte*) &val;
+    EEPROM.write(pos, *p);
+    EEPROM.write(pos + 1, *(p + 1));
+    EEPROM.write(pos + 2, *(p + 2));
+    EEPROM.write(pos + 3, *(p + 3));
+    EEPROM.commit();
+}
+
+//========================================
+int eeGetInt(int pos) {
+  int val;
+  byte* p = (byte*) &val;
+  *p        = EEPROM.read(pos);
+  *(p + 1)  = EEPROM.read(pos + 1);
+  *(p + 2)  = EEPROM.read(pos + 2);
+  *(p + 3)  = EEPROM.read(pos + 3);
+  return val;
 }
   
 //========================================
@@ -198,6 +253,46 @@ void callback(char* topic, byte* payload, unsigned int length) {
   mess = true;
  
 }
+
+//========================================
+//on veut sauvegarder par exemple le nombre décimal 55084, en binaire : 1101 0111 0010 1100
+
+//fonction d'écriture d'un type int en mémoire EEPROM
+void sauverInt(int adresse, int val) 
+{   
+    //découpage de la variable val qui contient la valeur à sauvegarder en mémoire
+    unsigned char faible = val & 0x00FF; //récupère les 8 bits de droite (poids faible) -> 0010 1100 
+    //calcul : 1101 0111 0010 1100 & 0000 0000 1111 1111 = 0010 1100
+
+    unsigned char fort = (val >> 8) & 0x00FF;  //décale puis récupère les 8 bits de gauche (poids fort) -> 1101 0111
+    //calcul : 1101 0111 0010 1100 >> 8 = 0000 0000 1101 0111 puis le même & qu’avant
+
+    //puis on enregistre les deux variables obtenues en mémoire
+    EEPROM.write(adresse, fort) ; //on écrit les bits de poids fort en premier
+    EEPROM.write(adresse+1, faible) ; //puis on écrit les bits de poids faible à la case suivante
+    EEPROM.commit();  
+
+  
+}
+ 
+//========================================
+//lecture de la variable de type int enregistrée précédemment par la fonction que l'on a créée
+
+int lireInt(int adresse)
+{
+    int val = 0 ; //variable de type int, vide, qui va contenir le résultat de la lecture
+
+    unsigned char fort = EEPROM.read(adresse);     //récupère les 8 bits de gauche (poids fort) -> 1101 0111
+    unsigned char faible = EEPROM.read(adresse+1); //récupère les 8 bits de droite (poids faible) -> 0010 1100
+
+    //assemblage des deux variable précédentes
+    val = fort ;         // val vaut alors 0000 0000 1101 0111
+    val = val << 8 ;     // val vaut maintenant 1101 0111 0000 0000 (décalage)
+    val = val | faible ; // utilisation du masque
+    // calcul : 1101 0111 0000 0000 | 0010 1100 = 1101 0111 0010 1100
+
+    return val ; //on n’oublie pas de retourner la valeur lue !
+}
  
 //========================================
 /* tente une reconnection au seveur mqtt */
@@ -224,6 +319,9 @@ void reconnect() {
       // ... and resubscribe
       client.subscribe("#");
       noconnect=true;
+      digitalWrite(lbp,HIGH);
+      delay(10000);
+      digitalWrite(lbp,LOW);
     } else {
       if ( debug ) {
         Serial.print("failed, rc=");
@@ -287,6 +385,8 @@ void loop() {
    // pinMode(lbp,OUTPUT);
     if ( sujet == "mod_lum/conf" ) {
       lum = mesg.toInt();
+ //     sauverInt(100, lum);
+      eeWriteInt(100, lum);
     }
     if ( sujet == "mod_lum/cmd" ) {
       if ( mesg == "ON" ) {
@@ -297,9 +397,11 @@ void loop() {
       }
     }
     if ( sujet == "mod_lum/haut" ) {
-      digitalWrite(haut,HIGH);
-      delay(5000);
-      digitalWrite(haut,LOW);
+      if ( mesg == "ON" ) {
+        digitalWrite(haut,HIGH);
+        delay(5000);
+        digitalWrite(haut,LOW);
+      }
     } 
     if ( sujet == "mod_lum/bas" ) {
       if ( mesg == "ON" ) {
@@ -308,6 +410,39 @@ void loop() {
         digitalWrite(bas,LOW);
       }
     } 
+    if ( sujet == "mod_lum/cmd" ) {
+      if ( mesg == "aff" ) {
+        digitalWrite(lbp,HIGH);
+        delay(5000);
+        digitalWrite(lbp,LOW);
+        snprintf (msg, 80, "Valeur de conf luminosité", String(lum).c_str());
+        client.publish("mod_lum/conflum", String(lum).c_str());  
+        if (debug) {
+          Serial.print("conf lum = ");
+          Serial.println(lum);   
+        }
+      }
+      if ( mesg == "tmp" ) {
+        digitalWrite(lbp,HIGH);
+        delay(5000);
+        digitalWrite(lbp,LOW);
+        snprintf (msg, 80, "Valeur de conf du temps", String(lsmg).c_str());
+        client.publish("mod_lum/conflum", String(lsmg).c_str());  
+        if (debug) {
+          Serial.print("conf lsmg = ");
+          Serial.println(lsmg);   
+        }
+      }
+    }
+    if ( sujet == "mod_lum/conftemps" ) {
+      lsmg = mesg.toInt();
+      eeWriteInt(50, lsmg);
+      if (debug) {
+        Serial.print("temps récupéré ");
+        Serial.println(lsmg);
+      }
+    }
+     
    // pinMode(lbp,INPUT);
     mess = false;
   }
@@ -315,7 +450,7 @@ void loop() {
  
   // renvoie le niveau de la ldr tous les 10 sec
   long now = millis();
-  if (now - lastMsg > 10000) {
+  if (now - lastMsg > lsmg) {
     lastMsg = now;
 //    value = value + 1;
 //    snprintf (msg, 50, "Envoi numéro #%ld", value);
@@ -330,22 +465,28 @@ void loop() {
     vin = (valeur * 3.3) / 1024.0; 
     
     snprintf (msg, 50, "Luminosité %ld", valeur);
-    client.publish("mod_lum", msg);
-    snprintf (msg, 50, "voltage %ld", vin);
-    client.publish("mod_lum", msg);
-    
+    client.publish("mod_lum", String(valeur).c_str());
+    client.publish("mod_lum/lum", msg);
+//    dtostrf(vin, 6, 2, floatmsg);
+//    PString(floatmsg, 10, vin);
+    snprintf (msg, 50, "voltage %ld", String(vin).c_str());
+    client.publish("mod_lum/volt", String(vin).c_str());
+    if (debug) {
+      Serial.print("string voltage ");
+      Serial.println(String(vin).c_str());
+    }
  
  
     if (debug) {
-      //Serial.print("valeur = ");
-      //Serial.println(valeur);   
-      //Serial.print("volt = ");
-      //Serial.println(vin); 
-      if (enbas) {
-        Serial.println("état volet = true");
-      } else {
-        Serial.println("état volet = false");
-      }
+      Serial.print("valeur = ");
+      Serial.println(valeur);   
+      Serial.print("volt = ");
+      Serial.println(vin); 
+//      if (enbas) {
+//        Serial.println("état volet = true");
+//      } else {
+//        Serial.println("état volet = false");
+//      }
     }
     
     if (( valeur < lum ) && (!enbas)) {
